30fb9ead7dafe43e69180de54ed00ca2259072cf
Thomas Cherry <thomas.a.cherry@nasa.gov>
2019-01-02 15:29:42 -0500

    Initial Commit

---
+++ Service Test/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Service Test
+//
+//  Created by Thomas Cherry on 2019-01-02.
+//  Copyright © 2019 Thomas Cherry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
